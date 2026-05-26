5401e9fe2d16df04c28c3c8d77db5c2d4db95afe
bopich <158606241+bopich228@users.noreply.github.com>
2026-05-26 23:49:19 +0300

    Add files via upload

---
+++ task2/eggogi.py
@@ -0,0 +1,8 @@
+class Error_ValueUnacceptable(Exception):
+    '''Ошибка: недопустимое значение'''
+    pass
+
+class Error_FieldNotFound(Exception):
+    '''Ошибка: обращение к несуществующему полю таблицы'''
+    pass
+
